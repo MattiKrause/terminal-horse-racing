7e24769af9f4bf4468d4973a41157ceb28bad2dd
Matti Krause <noemail>
2023-05-11 17:50:35 +0200

    made readme

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a little race game.
+Once the game is started six numbers representing horses will appear. 
+They will move each time `enter` is pressed. The previous state of the game should disappear, if that is supported by the terminal.
+
+### Installation
+rustc and cargo must both be installed. Then simply run `cargo build --release` and then `./target/release/horse-racing` 
+in the project directory
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+#![deny(clippy::pedantic)]
+
 use std::io::stdin;
 use std::process::exit;
-use std::time::{Instant, SystemTime};
+use std::time::{SystemTime};
 use term::color;
 
 static HORSE_COLORS: [color::Color; 5] = [color::RED, color::GREEN, color::MAGENTA, color::BLUE, color::CYAN];
+
 #[derive(Clone)]
 struct Horse { advance: u32 }
 
@@ -19,10 +22,10 @@ const WINNING_NUM: u32 = 50;
 const MAX_ADVANCE: u128 = 4;
 
 fn run_game(terminal: &mut term::StdoutTerminal) -> std::io::Result<()> {
-    use std::io::Read;
+
     let mut horses = vec![Horse { advance: 0 }; 7];
     write_horses(terminal, &horses).unwrap();
-    let mut stdin = stdin();
+    let stdin = stdin();
     let mut line = String::new();
     while horses.iter().all(|h| h.advance < WINNING_NUM) {
         let _ = stdin.read_line(&mut line);
@@ -39,6 +42,8 @@ fn run_game(terminal: &mut term::StdoutTerminal) -> std::io::Result<()> {
 }
 
 fn tick_horses(horses: &mut [Horse], tick_by: u128, max_advance: u128) {
+    assert!(max_advance <= u32::MAX as u128 - 1);
+
     let _ = horses.iter_mut().fold(tick_by, |acc, horse| {
         horse.advance += ((acc % max_advance) + 1) as u32;
         acc / max_advance
@@ -46,11 +51,13 @@ fn tick_horses(horses: &mut [Horse], tick_by: u128, max_advance: u128) {
 }
 
 fn write_winning_state(terminal: &mut term::StdoutTerminal, horses: &[Horse]) -> std::io::Result<()> {
-    let mut horses_by_advanced = to_horse_iter(&horses).collect::<Vec<_>>();
+    let mut horses_by_advanced = to_horse_iter(horses).collect::<Vec<_>>();
     horses_by_advanced.sort_by(|(_, h1,_), (_, h2, _)| h1.advance.cmp(&h2.advance).reverse());
+
     let (winner_horse, _, winning_horse_color) = *horses_by_advanced.first().unwrap();
+
     delete_horses(terminal, horses_by_advanced.len())?;
-    write_horses_iter(terminal, horses_by_advanced.into_iter())?;
+    write_horses_iter(terminal, true, horses_by_advanced.into_iter())?;
     let _ = terminal.fg(winning_horse_color);
     writeln!(terminal, "horse {winner_horse} won!")
 }
@@ -64,11 +71,22 @@ fn delete_horses(terminal: &mut term::StdoutTerminal, horse_count: usize)-> std:
     Ok(())
 }
 
-fn write_horses_iter<'a>(term: &mut term::StdoutTerminal,mut horses: impl Iterator<Item = (usize, &'a Horse, color::Color)>) -> std::io::Result<()> {
-    horses.try_for_each(|(horse_number, advance, c)| {
+fn write_horses_iter<'a>(term: &mut term::StdoutTerminal, do_write_finish_line: bool, mut horses: impl Iterator<Item = (usize, &'a Horse, color::Color)>) -> std::io::Result<()> {
+    horses.try_for_each(|(horse_number, horse, c)| {
         let _ = term.fg(c);
-        writeln!(term, "{}{}", "_".repeat(advance.advance as usize), horse_number)
+        write!(term, "{}{}", "_".repeat(horse.advance as usize), horse_number)?;
+        if do_write_finish_line {
+            write_finish_line(term, horse.advance)?;
+        }
+        writeln!(term)
     })
+}
+
+fn write_finish_line(term: &mut term::StdoutTerminal, advance: u32) -> std::io::Result<()> {
+    let Some(until_winning_line) = WINNING_NUM.checked_sub(advance).and_then(|n| n.checked_sub(1)) else {
+        return Ok(())
+    };
+    write!(term, "{}|", " ".repeat(until_winning_line as usize))
 }
 
 fn to_horse_iter(horses: &[Horse]) -> impl Iterator<Item=(usize, &'_ Horse, color::Color)> {
@@ -76,5 +94,5 @@ fn to_horse_iter(horses: &[Horse]) -> impl Iterator<Item=(usize, &'_ Horse, colo
 }
 
 fn write_horses(term: &mut term::StdoutTerminal, horses: &[Horse]) -> std::io::Result<()> {
-    write_horses_iter(term, to_horse_iter(horses))
+    write_horses_iter(term, true, to_horse_iter(horses))
 }
